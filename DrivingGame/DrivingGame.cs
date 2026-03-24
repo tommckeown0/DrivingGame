f582d95e6254f9cef9a18361fde6d9a40a441379
tom <tommckeown909@gmail.com>
2026-03-24 21:09:25 +0000

    make the cube more car-like

---
+++ DrivingGame/DrivingGame.cs
@@ -0,0 +1,108 @@
+﻿using Raylib_cs;
+using System.Numerics;
+
+public class DrivingGame
+{
+    private Vector3 carPosition;
+    private float carRotation;
+    private Camera3D camera;
+    private float speed = 0.0005f;
+    Model carModel = Raylib.LoadModelFromMesh(Raylib.GenMeshCube(2, 2, 2));
+    private float currentSpeed = 0f;
+    private const float acceleration = 0.000001f;
+    private const float maxSpeed = 0.001f;
+    private const float friction = 0.9999f;
+
+    public void Init()
+    {
+        camera.Position = new Vector3(0, 10, 10);
+        carPosition = new Vector3(0, 0, 0);
+        carRotation = 0;
+        camera.Target = carPosition;
+        camera.Up = new Vector3(0, 1, 0);
+        camera.FovY = 45;
+        camera.Projection = CameraProjection.Perspective;
+    }
+
+    public void Run()
+    {
+        while (!Raylib.WindowShouldClose())
+        {
+            Update();
+            Draw();
+        }
+    }
+
+    public void Update()
+    {
+        Vector3 forward = new Vector3(
+            MathF.Sin(carRotation),
+            0,
+            MathF.Cos(carRotation)
+        );
+        Vector3 backward = new Vector3(
+            -MathF.Sin(carRotation),
+            0,
+            -MathF.Cos(carRotation)
+        );
+        if (Raylib.IsKeyDown(KeyboardKey.W))
+        {
+            //carPosition += forward * currentSpeed;
+            currentSpeed += acceleration;
+        }
+        else if (Raylib.IsKeyDown(KeyboardKey.S))
+        {
+            //carPosition += backward * currentSpeed;
+            currentSpeed -= acceleration;
+        }        
+        else { 
+            currentSpeed *= friction;
+        }
+        if (Raylib.IsKeyDown(KeyboardKey.A))
+        {
+            carRotation += 0.0002f;
+        }
+        if (Raylib.IsKeyDown(KeyboardKey.D))
+        {
+            carRotation -= 0.0002f;
+        }
+        carPosition += forward * currentSpeed;
+        camera.Target = carPosition;
+        camera.Position = carPosition + new Vector3(0, 5, 10);
+        //currentSpeed *= friction;
+    }
+
+    public void Draw()
+    {
+        Raylib.BeginDrawing();
+        Raylib.ClearBackground(Color.RayWhite);
+        Raylib.DrawText("Speed: " + currentSpeed.ToString("F4"), 10, 10, 20, Color.Black);
+
+        Raylib.BeginMode3D(camera);
+        Vector3 forward = new Vector3(MathF.Sin(carRotation), 0, MathF.Cos(carRotation));        
+        Raylib.DrawLine3D(carPosition, carPosition + forward * 10, Color.Blue);
+        Raylib.DrawGrid(1000, 1);
+        Raylib.DrawModelEx(
+            carModel,
+            carPosition,
+            new Vector3(0, 1, 0),  // rotation axis (Y axis)
+            carRotation * (180f / MathF.PI),  // rotation angle in DEGREES
+            new Vector3(1, 1, 1),  // scale
+            Color.Red
+        );
+        Raylib.DrawModelWiresEx(
+            carModel,
+            carPosition,
+            new Vector3(0, 1, 0),  // rotation axis (Y axis)
+            carRotation * (180f / MathF.PI),  // rotation angle in DEGREES
+            new Vector3(1, 1, 1),  // scale
+            Color.Black
+        );
+        Raylib.DrawLine3D(new Vector3(0, 0, 0), new Vector3(0, 5, 0), Color.Green);
+        Raylib.DrawLine3D(new Vector3(0, 0, 0), new Vector3(5, 0, 0), Color.Red);
+        Raylib.DrawLine3D(new Vector3(0, 0, 0), new Vector3(0, 0, 5), Color.Blue);        
+        Raylib.EndMode3D();
+
+        Raylib.EndDrawing();
+    }
+}
--- DrivingGame/Program.cs
+++ DrivingGame/Program.cs
@@ -1,68 +1,11 @@
 ﻿using Raylib_cs;
-using System.Numerics;
 
 Raylib.InitWindow(800, 600, "Driving Game");
 
-Camera3D camera = new Camera3D();
-camera.Position = new Vector3(0, 10, 0);
-camera.Target = new Vector3(0, 0, 0);
-camera.Up = new Vector3(0, 1, 0);
-camera.FovY = 45;
-camera.Projection = CameraProjection.Perspective;
-
-while (!Raylib.WindowShouldClose())
-{
-    Raylib.BeginDrawing();
-    Raylib.ClearBackground(Color.RayWhite);
-
-    if (Raylib.IsKeyDown(KeyboardKey.W))
-    {
-        camera.Position += new Vector3(-0.001f, 0, 0);
-        //camera.Target += new Vector3(-0.001f, 0, 0);
-    }
-    if (Raylib.IsKeyDown(KeyboardKey.S))
-    {
-        camera.Position += new Vector3(0.001f, 0, 0);
-        //camera.Target += new Vector3(0.001f, 0, 0);
-    }
-    if (Raylib.IsKeyDown(KeyboardKey.A))
-    {
-        camera.Position += new Vector3(0, 0, -0.001f);
-        //camera.Target += new Vector3(0, 0, -0.001f);
-    }
-    if(Raylib.IsKeyDown(KeyboardKey.D))
-    {
-        camera.Position += new Vector3(0, 0, 0.001f);
-        //camera.Target += new Vector3(0, 0, 0.001f);
-    }
-    if (Raylib.IsKeyDown(KeyboardKey.Q))
-    {
-        camera.Position += new Vector3(0, -0.001f, 0);
-        //camera.Target += new Vector3(0, -0.001f, 0);
-    }
-    if (Raylib.IsKeyDown(KeyboardKey.E))
-    {
-        camera.Position += new Vector3(0, 0.001f, 0);
-        //camera.Target += new Vector3(0, 0.001f, 0);
-    }
-
-    Raylib.DrawText("Position X value: " + camera.Position.X.ToString("F3"), 10, 10, 20, Color.Black);
-    Raylib.DrawText("Position Y value: " + camera.Position.Y.ToString("F2"), 10, 30, 20, Color.Black);
-    Raylib.DrawText("Position Z value: " + camera.Position.Z.ToString("F2"), 10, 50, 20, Color.Black);
-    Raylib.DrawText("Target X value: " + camera.Target.X.ToString("F3"), 10, 70, 20, Color.Black);
-    Raylib.DrawText("Target Y value: " + camera.Target.Y.ToString("F2"), 10, 90, 20, Color.Black);
-    Raylib.DrawText("Target Z value: " + camera.Target.Z.ToString("F2"), 10, 110, 20, Color.Black);
-
-    Raylib.BeginMode3D(camera);
-    Raylib.DrawGrid(10, 1);
-    Raylib.DrawCube(new Vector3(0, 0, 0), 2, 2, 2, Color.Red);
-    Raylib.DrawCubeWires(new Vector3(0, 0, 0), 2, 2, 2, Color.Black);
-    Raylib.DrawLine3D(new Vector3(0, 0, 0), new Vector3(0, 5, 0), Color.Green);
-    Raylib.DrawLine3D(new Vector3(0, 0, 0), new Vector3(5, 0, 0), Color.Red);
-    Raylib.DrawLine3D(new Vector3(0, 0, 0), new Vector3(0, 0, 5), Color.Blue);
-    Raylib.EndMode3D();
-
-    Raylib.EndDrawing();
-}
+//CameraDemo.Run();
+//DrivingGame.Run();
+DrivingGame game = new DrivingGame();
+game.Init();
+game.Run();
 
 Raylib.CloseWindow();
